7b96e1b0f5a79c22e075d94f886c3c64a638792d
shoeree <sterling.hoeree@gmail.com>
2014-07-06 19:04:23 -0700

    added some more fabfile methods

--- fabfile.py
+++ fabfile.py
@@ -3,34 +3,48 @@ from fabric.api import lcd, local
 
 DEV_ROOT = '/home/shoeree/projects/vla/'
 APP_ROOT = '/app/django.vancouverlifeguards.com/vla/'
-GIT_URL  = 'https://github.com/shoeree/vla.git'
+GIT_URL  = 'git@github.com:shoeree/vla.git'
 
-def prepare_deployment():
+def prepare():
     with lcd(DEV_ROOT):
         local('python manage.py test vla')
         local('git add -p && git commit')
-        local('git push origin -- master') # TODO: have a dev branch
+        local('git push origin -- dev')
+        local('git checkout master')
+        local('git merge dev')
+        local('python manage.py test vla')
 
 def start_uwsgi():
     with lcd(APP_ROOT):
         local('uwsgi --ini uwsgi.ini')
+
+def stop_uwsgi():
+    with lcd(APP_ROOT):
+        uwsgi_ini = open('uwsgi.ini')
+        pid_file = [line.split('=')[1] for line in uwsgi_ini.readlines() if line.startswith('pidfile')][0]
+        local('uwsgi --stop %s' % pid_file)
+        uwsgi_ini.close()
 
 def reload_uwsgi():
     with lcd(APP_ROOT):
         uwsgi_ini = open('uwsgi.ini')
         pid_file = [line.split('=')[1] for line in uwsgi_ini.readlines() if line.startswith('pidfile')][0]
         local('kill -HUP $(cat %s)' %pid_file)
+        uwsgi_ini.close()
+
+def reload_webserver():
+    local('kill -HUP $(cat /var/run/nginx.pid)')
 
 def deploy():
+    with lcd(DEV_ROOT):
+        local('git checkout master')
+        local('git push origin -- master')
+        
     with lcd(APP_ROOT):
         # pull most recent commits
-        local('git pull')
+        local('git pull origin -- master')
         local('cp %s/vla/secret_settings.py %s/vla/' % (DEV_ROOT, APP_ROOT))
         local('python manage.py migrate volition')
         local('python manage.py test volition')
         reload_uwsgi()
         reload_webserver()
-
-def reload_webserver():
-    local('kill -HUP $(cat /var/run/nginx.pid)')
-
